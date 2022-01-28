21c3bc6ee499a0c3e79013f61d6f769d70c43696
butdory <>
2022-01-28 12:06:00 +0900

    footer bux fix

--- src/components/semantic/LectureFooter.js
+++ src/components/semantic/LectureFooter.js
@@ -18,6 +18,9 @@ const LectureFooterStyle = styled.div`
     .LectureFooter_list_li{
         flex: 0.03;
     }
+    .pointer{
+        cursor: pointer;
+    }
 `
 
 const LectureFooter = (props) => {
@@ -27,6 +30,7 @@ const LectureFooter = (props) => {
     let bool = false;
     let leftArrowKey = '';
     let rightArrowKey = '';
+    let endCheck = 0;
 
     useEffect(() => {
         GetDatabase(setData);
@@ -42,15 +46,17 @@ const LectureFooter = (props) => {
             }
             i++;
             if(key2 === props.key2){
-                arr.unshift(<a href={`/Lecture${props.key1}/${leftArrowKey}`}><img src="/img/leftArrow.png"></img></a>)
+                {i === 1 ? arr.unshift(<img className="pointer" src="/img/leftArrow.png"></img>) : arr.unshift(<a href={`/Lecture${props.key1}/${leftArrowKey}`}><img src="/img/leftArrow.png"></img></a>)}
                 arr.push(<li className="LectureFooter_list_li"><a className="current" href={`/Lecture${props.key1}/${key2}`}>{i}</a></li>);
                 bool = true;
+                endCheck = i;
                 continue
             }
             arr.push(<li className="LectureFooter_list_li"><a href={`/Lecture${props.key1}/${key2}`}>{i}</a></li>);
             leftArrowKey = key2;
         }
-        arr.push(<a href={`/Lecture${props.key1}/${rightArrowKey}`}><img src="/img/rightArrow.png"></img></a>);
+        {endCheck === i ? arr.push(<img className="pointer" src="/img/rightArrow.png"></img>) : arr.push(<a href={`/Lecture${props.key1}/${rightArrowKey}`}><img src="/img/rightArrow.png"></img></a>)}
+        
     }
 
     return(
